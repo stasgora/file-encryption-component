19f5013a0686429818e760c069e8592445a9398b
Stanisław Góra <stasgora@gmail.com>
2019-04-27 14:38:42 +0200

    file params

--- src/main/java/edu/file/encryption/component/model/EncryptionParameters.java
+++ src/main/java/edu/file/encryption/component/model/EncryptionParameters.java
@@ -3,5 +3,7 @@ package edu.file.encryption.component.model;
 public class EncryptionParameters {
 
 	public String initialVector = "completelySecure";
+	public String fileName;
+	public int fileLength;
 
 }
