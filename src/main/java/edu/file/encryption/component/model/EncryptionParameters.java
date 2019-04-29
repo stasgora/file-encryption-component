8585aba3b274149e56af99d87d2c12064bc587f7
Jan Kruczyński <jan.kruczynski16@gmail.com>
2019-04-29 20:22:41 +0200

    Shh nothing happend

--- src/main/java/edu/file/encryption/component/model/EncryptionParameters.java
+++ src/main/java/edu/file/encryption/component/model/EncryptionParameters.java
@@ -14,14 +14,11 @@ public class EncryptionParameters {
 	public CipherAlgorithmMode cipherAlgMode;
 	public String hashFunctionName = "SHA-256";
 
-<<<<<<< HEAD
 	public String keyDir = "TotallyNotKeys";
 	public String privateKeyDir = "NotPrivateKeyDirectory";
 	public String publicKeySuffix = "Public.key";
 	public String privateKeySuffix = "Private.key";
-=======
-	public String recipient;
->>>>>>> 2eaac06bfa18d059a1f810cda001feb8ec99af1a
+	
 	public String fileName;
 	public int fileLength;
 
